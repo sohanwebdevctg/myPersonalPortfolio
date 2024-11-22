0330251be347f66041070de4fc6b5a06b0561383
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-11-22 18:45:28 +0600

    creating personal portfoilo setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>my personal portfolio</h1>
+      <button className="btn btn-neutral">Neutral</button>
+    </div>
+  );
+};
+
+export default App;
