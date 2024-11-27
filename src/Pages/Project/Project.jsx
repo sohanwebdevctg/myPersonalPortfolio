fba99a0193d1712bc18a1dbc8fb4aa56c4dec190
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-11-27 19:22:26 +0600

    change project page color

--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Title from "../../Components/Title/Title";
 import Loading from "../../Components/Loading/Loading";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { AuthContext } from './../../Components/Providers/Providers';
 
 
 const Project = () => {
@@ -13,6 +14,8 @@ const Project = () => {
   useEffect(() => {
     AOS.init();
   },[]);
+
+  const {color} = useContext(AuthContext);
 
   // all state function data
   const [previous, setPrevious] = useState([]);
@@ -53,18 +56,18 @@ const Project = () => {
     {/* title section end */}
       {/* content section start */}
       <div className="container mx-auto py-10 h-full xl:h-full">
-        <div className="flex flex-col sm:flex-row justify-between bg-black shadow-inner shadow-slate-600 p-5 gap-2 xl:gap-10">
+        <div className={`flex flex-col sm:flex-row justify-between ${color ? 'bg-white shadow-inner shadow-slate-300' : 'bg-black shadow-inner shadow-slate-600'}  p-5 gap-2 xl:gap-10`}>
           {/* title start */}
           <div>
-            <h2 className="text-lg sm:text-lg md:text-xl lg:text-[22px] xl:text-2xl 2xl:text-3xl font-bold text-slate-200">My <span className="text-yellow-400">Project</span>
+            <h2 className={`text-lg sm:text-lg md:text-xl lg:text-[22px] xl:text-2xl 2xl:text-3xl font-bold ${color ? 'text-black' : 'text-slate-200'}`}>My <span className={`${color ? 'text-red-500' : 'text-yellow-400'}`}>Project</span>
             </h2>
           </div>
           {/* title end */}
           {/* button start */}
           <ul className=" flex items-center gap-5 sm:gap-3 md:gap-3 lg:gap-4">
-            <li><button onClick={() => buttonFun('html/css')} className="btn btn-xs md:btn-xs lg:btn-sm xl:btn-sm w-16 sm:w-20 md:w-24 lg:w-28 xl:w-36 2xl:w-40 text-white rounded-sm bg-yellow-400 hover:bg-yellow-400 text-[8px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base">HTML/CSS</button></li>
-            <li><button onClick={() => buttonFun('javaScript/dom')} className="btn btn-xs md:btn-xs lg:btn-sm xl:btn-sm w-16 sm:w-20 md:w-24 lg:w-28 xl:w-36 2xl:w-40 text-white rounded-sm bg-yellow-400 hover:bg-yellow-400 text-[8px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base">JavaScript/DOM</button></li>
-            <li><button onClick={() => buttonFun('react')} className="btn btn-xs md:btn-xs lg:btn-sm xl:btn-sm w-16 sm:w-20 md:w-24 lg:w-28 xl:w-36 2xl:w-40 text-white rounded-sm bg-yellow-400 hover:bg-yellow-400 text-[8px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base">REACT</button></li>
+            <li><button onClick={() => buttonFun('html/css')} className={`btn btn-xs md:btn-xs lg:btn-sm xl:btn-sm w-16 sm:w-20 md:w-24 lg:w-28 xl:w-36 2xl:w-40 text-white rounded-sm ${color ? 'bg-red-500 hover:bg-red-500' :'bg-yellow-400 hover:bg-yellow-400'} text-[8px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base`}>HTML/CSS</button></li>
+            <li><button onClick={() => buttonFun('javaScript/dom')} className={`btn btn-xs md:btn-xs lg:btn-sm xl:btn-sm w-16 sm:w-20 md:w-24 lg:w-28 xl:w-36 2xl:w-40 text-white rounded-sm ${color ? 'bg-red-500 hover:bg-red-500' :'bg-yellow-400 hover:bg-yellow-400'} text-[8px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base`}>JavaScript/DOM</button></li>
+            <li><button onClick={() => buttonFun('react')} className={`btn btn-xs md:btn-xs lg:btn-sm xl:btn-sm w-16 sm:w-20 md:w-24 lg:w-28 xl:w-36 2xl:w-40 text-white rounded-sm ${color ? 'bg-red-500 hover:bg-red-500' :'bg-yellow-400 hover:bg-yellow-400'} text-[8px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base`}>REACT</button></li>
           </ul>
           {/* button end */}
         </div>
@@ -80,22 +83,22 @@ const Project = () => {
                 {/* top start */}
               <div className="p-3 sm:p-3 md:p-3 lg:p-4 xl:p-5 2xl:p-6">
                 <div className="space-y-2">
-                  <h3 className="text-yellow-400 font-bold text-base sm:text-base md:text-lg lg:text-[19px] xl:text-xl 2xl:text-2xl">{data.projectName}</h3>
+                  <h3 className={`${color ? 'text-red-500' : 'text-yellow-400'} font-bold text-base sm:text-base md:text-lg lg:text-[19px] xl:text-xl 2xl:text-2xl`}>{data.projectName}</h3>
                   <p className="text-[9px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-[13px] 2xl:text-sm text-gray-500 text-justify">{data.details}</p>
                 </div>
               </div>
               {/* top end */}
               {/* bottom start */}
               <div className="p-3 sm:p-3 md:p-3 lg:p-4 xl:p-5 2xl:p-6 space-y-2">
-                <h3 className="text-yellow-400 font-bold text-base sm:text-base md:text-lg lg:text-[19px] xl:text-xl 2xl:text-2xl">Feature</h3>
+                <h3 className={`${color ? 'text-red-500' : 'text-yellow-400'} font-bold text-base sm:text-base md:text-lg lg:text-[19px] xl:text-xl 2xl:text-2xl`}>Feature</h3>
                 <ul className="space-y-1">{data.feature.map((item, index) =>   <li key={index} className="text-[9px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-[13px] 2xl:text-sm text-gray-500 text-justify">{++index + '.'} {item}</li>)}</ul>
               </div>
               {/* bottom end */}
               {/* link section start */}
               <div className="p-3 sm:p-3 md:p-3 lg:p-4 xl:p-5 2xl:p-6">
                 <ul className="flex justify-between items-center">
-                  <li><Link to={data.codeLink}><button className="btn btn-xs text-[9px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base hover:bg-yellow-500 bg-yellow-500 text-white rounded-sm">Code Link</button></Link></li>
-                  <li><Link to={data.liveLink}><button className="btn btn-xs text-[9px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base hover:bg-yellow-500 bg-yellow-500 text-white rounded-sm">Live Link</button></Link></li>
+                  <li><Link to={data.codeLink}><button className={`btn btn-xs text-[9px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base ${color ? 'hover:bg-red-500 bg-red-500' : 'hover:bg-yellow-500 bg-yellow-500'} text-white rounded-sm`}>Code Link</button></Link></li>
+                  <li><Link to={data.liveLink}><button className={`btn btn-xs text-[9px] sm:text-[10px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base ${color ? 'hover:bg-red-500 bg-red-500' : 'hover:bg-yellow-500 bg-yellow-500'} text-white rounded-sm`}>Live Link</button></Link></li>
                 </ul>
               </div>
               {/* link section end */}
